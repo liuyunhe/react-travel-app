7775d66002c02d34f848796e47edb0e677f9c804
liuyunhe <liuyunhe666@aliyun.com>
2021-07-17 17:49:21 +0800

    基础路由系统，页面导航

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import styles from './App.module.css'
-import { BrowserRouter, Route } from 'react-router-dom'
-import { HomePage } from './pages'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { DetailPage, HomePage, RegisterPage, SignInPage } from './pages'
 
 const App: React.FC = () => {
   return (
     <div className={styles.App}>
       <BrowserRouter>
-        <Route path="/" component={HomePage}></Route>
+        {/* <Switch>: 路径的切换以页面为单位，不要页面堆叠  */}
+        <Switch>
+          {/* exact: 精确 */}
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/signIn" component={SignInPage}></Route>
+          <Route path="/register" component={RegisterPage}></Route>
+          <Route path="/detail/:touristRouteId" component={DetailPage}></Route>
+          <Route render={() => <h1>404 not found,页面去火星了</h1>}></Route>
+        </Switch>
       </BrowserRouter>
     </div>
   )
