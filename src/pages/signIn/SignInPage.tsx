26e786159e648a1ff1269c014954e644d7548c11
liuyunhe <liuyunhe666@aliyun.com>
2021-09-01 15:56:36 +0800

    登录页面

--- src/pages/signIn/SignInPage.tsx
+++ src/pages/signIn/SignInPage.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { UserLayout } from '../../layout/userLayout'
+import { SignInForm } from './SignInForm'
 
 export const SignInPage: React.FC = (props) => {
   console.log(props)
   return (
     <UserLayout>
-      <h1>登录页面</h1>
+      <SignInForm />
     </UserLayout>
   )
 }
