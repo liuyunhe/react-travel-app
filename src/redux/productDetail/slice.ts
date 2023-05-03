f5d21e2a351a6f09961c2b97d63057b41dcd6cb7
liuyunhe <liuyunhe666@aliyun.com>
2023-05-03 11:16:20 +0800

    增加注释

--- src/redux/productDetail/slice.ts
+++ src/redux/productDetail/slice.ts
@@ -16,6 +16,15 @@ const initialState: ProductDetailState = {
 export const getProductDetail = createAsyncThunk(
   'productDetail/getProductDetail',
   async (touristRouteId: string, thunkApi) => {
+    //   thunkApi.dispatch(productDetailSlice.actions.fetchStart())
+    //   try {
+    //     const { data } = await axios.get(`/api/touristRoutes/${touristRouteId}`)
+    //     thunkApi.dispatch(productDetailSlice.actions.fetchSuccess(data))
+    //   } catch (e) {
+    //     thunkApi.dispatch(productDetailSlice.actions.fetchFail(e))
+    //   }
+    // }
+    //返回promise自动处理life状态
     const { data } = await axios.get(`/api/touristRoutes/${touristRouteId}`)
     return data
   }
@@ -25,6 +34,8 @@ export const productDetailSlice = createSlice({
   name: 'productDetail',
   initialState,
   reducers: {
+    // 这里reducer 和 action 捆绑在一起
+    // 每个函数对应一个action
     fetchStart: (state) => {
       // return { ...state, loading: true }
       // immer
@@ -41,6 +52,7 @@ export const productDetailSlice = createSlice({
       state.error = action.payload
     }
   },
+  // promise life action
   extraReducers: {
     [getProductDetail.pending.type]: (state) => {
       // return { ...state, loading: true }
